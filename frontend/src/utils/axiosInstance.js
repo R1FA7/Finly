d8b2473dc291867bc3817a9b3b4c0f60b09185bf
R1FA7 <minhazulkabir510@gmail.com>
2025-09-20 00:38:52 +0600

    update axiosInstance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -25,6 +25,47 @@ axiosInstance.interceptors.request.use(
 )
 
 //Resonpose Interceptor : catch 401 and renew/ refresh access token
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) { // first time 401 error
+      originalRequest._retry = true;
+      try {
+        const { data } = await axiosInstance.post(
+          API_PATHS.AUTH.REFRESH_ACCESS_TOKEN,
+          {},
+          { withCredentials: true }
+        );
+
+        if (data.access_token) {
+          localStorage.setItem("access_token", data.access_token);
+          originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch (err) {
+        console.error("Refresh token failed", err);
+        window.location.href = '/login';
+      }
+    }
+
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error. Please try again later");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timed out. Please try again.");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 
 // axiosInstance.interceptors.response.use(
 //   (response)=>{
@@ -64,51 +105,51 @@ axiosInstance.interceptors.request.use(
 //     return Promise.reject(error)
 //   }
 // )
-axiosInstance.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   response => response,
+//   async error => {
+//     const originalRequest = error.config;
 
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      !originalRequest.url.includes(API_PATHS.AUTH.REFRESH_ACCESS_TOKEN)
-    ) {
-      originalRequest._retry = true;
+//     if (
+//       error.response?.status === 401 &&
+//       !originalRequest._retry &&
+//       !originalRequest.url.includes(API_PATHS.AUTH.REFRESH_ACCESS_TOKEN)
+//     ) {
+//       originalRequest._retry = true;
 
-      try {
-        const { data } = await axiosInstance.post(API_PATHS.AUTH.REFRESH_ACCESS_TOKEN, {}, { withCredentials: true });
+//       try {
+//         const { data } = await axiosInstance.post(API_PATHS.AUTH.REFRESH_ACCESS_TOKEN, {}, { withCredentials: true });
 
-        if (data.access_token) {
-          localStorage.setItem("access_token", data.access_token);
-          originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
-          return axiosInstance(originalRequest);
-        }
-      } catch (err) {
-        console.error("Refresh token failed", err);
-        localStorage.removeItem("access_token");
-        if (!window.location.pathname.startsWith('/login') && !window.location.pathname.startsWith('/register')) {
-          window.location.href = '/login';
-        }
-        return Promise.reject(err);
-      }
-    }
+//         if (data.access_token) {
+//           localStorage.setItem("access_token", data.access_token);
+//           originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+//           return axiosInstance(originalRequest);
+//         }
+//       } catch (err) {
+//         console.error("Refresh token failed", err);
+//         localStorage.removeItem("access_token");
+//         if (!window.location.pathname.startsWith('/login') && !window.location.pathname.startsWith('/register')) {
+//           window.location.href = '/login';
+//         }
+//         return Promise.reject(err);
+//       }
+//     }
 
-    if (error.response) {
-      if (error.response.status === 401) {
-        localStorage.removeItem("access_token");
-        if (!window.location.pathname.startsWith('/login') && !window.location.pathname.startsWith('/register')) {
-          window.location.href = "/login";
-        }
-      } else if (error.response.status === 500) {
-        console.error("Server error. Please try again later");
-      }
-    } else if (error.code === "ECONNABORTED") {
-      console.error("Request timed out. Please try again.");
-    }
+//     if (error.response) {
+//       if (error.response.status === 401) {
+//         localStorage.removeItem("access_token");
+//         if (!window.location.pathname.startsWith('/login') && !window.location.pathname.startsWith('/register')) {
+//           window.location.href = "/login";
+//         }
+//       } else if (error.response.status === 500) {
+//         console.error("Server error. Please try again later");
+//       }
+//     } else if (error.code === "ECONNABORTED") {
+//       console.error("Request timed out. Please try again.");
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance
