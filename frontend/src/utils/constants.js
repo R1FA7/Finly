7f6d29b1f36c22982a0c3f418cff66295f3ed6e2
R1FA7 <minhazulkabir510@gmail.com>
2025-09-19 23:17:06 +0600

    update vite url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,7 @@
 export const LOGO_URL = "https://img.freepik.com/premium-vector/budget-icon-financial-document-personal-bank_946691-1211.jpg"
 //https://img.freepik.com/premium-vector/restaurant-logo-design_617585-671.jpg
-export const VITE_BACKEND_URL = 'finly-production-a28a.up.railway.app'
+export const VITE_BACKEND_URL = 'https://finly-production-79f0.up.railway.app'
+
 
 
 //'http://localhost:3000'
